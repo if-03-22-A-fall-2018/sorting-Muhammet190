8728ca1600abd682cbbcc27318795b769a2bf6a6
Halil Özdogan <superman.batboy@outlook.com>
2019-03-11 19:07:36 +0100

    finish

--- stopwatch.cpp
+++ stopwatch.cpp
@@ -10,3 +10,22 @@
  * <your description here>
  *-----------------------------------------------------------------------------
 */
+#include "stopwatch.h"
+#include <stdio.h>
+#include <ctime>
+clock_t start, end =0;
+
+void start_stopwatch(){
+  start = clock();
+  end = 0;
+}
+void stop_stopwatch(){
+  end = clock();
+}
+float elapsed_time(){
+  if(end == 0)
+  {
+    stop_stopwatch();
+  }
+  return float(end-start) / CLOCKS_PER_SEC * 1000;
+  }
